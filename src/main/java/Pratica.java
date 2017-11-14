ae15c6f7c0f33f776c428cd0ebf30513daf2ae0c
Woich <minewoichbr@gmail.com>
2017-11-14 14:15:04 -0200

    Main

--- src/main/java/Pratica.java
+++ src/main/java/Pratica.java
@@ -1,3 +1,7 @@
+
+import utfpr.ct.dainf.pratica.PoligonalFechada;
+import utfpr.ct.dainf.pratica.PontoXZ;
+
 /**
  * UTFPR - Universidade Tecnológica Federal do Paraná
  * DAINF - Departamento Acadêmico de Informática
@@ -5,9 +9,11 @@
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
  */
 public class Pratica {
-
     public static void main(String[] args) {
-
+        PoligonalFechada<PontoXZ> poligono =  new PoligonalFechada<>();
+        poligono.set(0, new PontoXZ(-3,2));
+        poligono.set(1, new PontoXZ(-3,6));
+        poligono.set(2, new PontoXZ(0,2));
+        System.out.println("Comprimento da poligonal = "+poligono.getComprimento());
     }
-    
 }
